bd1090577e8dd724a2413f99808f3a80be927d78
victor eboso <victorlindsey8@gmail.com>
2018-07-16 10:45:32 +0300

    Added code to js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ var melan = function(Number)
 { for(Number=1;Number<30;Number ++){
   if(Number%3===0 && Number%5===0){ alert("pingpong");
 }else if(Number%3===0){ alert("ping");
-}else if(Number%5===0)
+}else if(Number%5===0){
+                  alert("pong")
+                             }else{ alert(Numbers)};};};
+$(document).ready(function)  {
+  $("form#number") .submit(function(event){
+    event.preventDefault();
+    var Number=parseInt($("input#put").val());
+    var Result=deter(Number)
+    $("ul#peter").append("<li>Result</li>");
+  });
+});
