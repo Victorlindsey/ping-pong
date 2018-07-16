57289664ade7798ff0aa17940f2f737ed1f00966
victor eboso <victorlindsey8@gmail.com>
2018-07-16 09:35:57 +0300

    Added a submit button

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var melan = function(Number)
+{ for(Number=1;Number<30;Number ++){
+  if(Number%3===0 && Number%5===0){ alert("pingpong");
+}else if(Number%3===0){ alert("ping");
+}else if(Number%5===0)
